e92cdc0bc97c7176e492b3a8c063c066f23c0ee6
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-03-05 18:27:14 -0300

    feat: add security configuration and state machine for loan processing

---
+++ loan-service/src/main/java/com/marcelo/loan/config/LoanStateMachineConfig.java
@@ -0,0 +1,41 @@
+package com.marcelo.loan.config;
+
+import com.marcelo.loan.entity.enums.LoanEvent;
+import com.marcelo.loan.entity.enums.LoanStatus;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachine
+public class LoanStateMachineConfig extends StateMachineConfigurerAdapter<LoanStatus, LoanEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<LoanStatus, LoanEvent> states) throws Exception {
+        states.withStates()
+                .initial(LoanStatus.PENDING)
+                .states(EnumSet.allOf(LoanStatus.class))
+                .end(LoanStatus.APPROVED)
+                .end(LoanStatus.REJECTED)
+                .end(LoanStatus.CANCELLED);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<LoanStatus, LoanEvent> transitions) throws Exception {
+        transitions
+                .withExternal()
+                .source(LoanStatus.PENDING).target(LoanStatus.UNDER_ANALYSIS).event(LoanEvent.SUBMIT)
+                .and()
+                .withExternal()
+                .source(LoanStatus.PENDING).target(LoanStatus.CANCELLED).event(LoanEvent.CANCEL)
+                .and()
+                .withExternal()
+                .source(LoanStatus.UNDER_ANALYSIS).target(LoanStatus.APPROVED).event(LoanEvent.APPROVE)
+                .and()
+                .withExternal()
+                .source(LoanStatus.UNDER_ANALYSIS).target(LoanStatus.REJECTED).event(LoanEvent.REJECT);
+    }
+}
