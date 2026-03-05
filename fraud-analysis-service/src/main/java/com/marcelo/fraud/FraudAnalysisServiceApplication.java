4f7cf8fe4d4cacdac45639e6ed4019b34abc1606
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-03-05 12:50:42 -0300

    first commit

---
+++ fraud-analysis-service/src/main/java/com/marcelo/fraud/FraudAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudAnalysisServiceApplication.class, args);
+	}
+
+}
