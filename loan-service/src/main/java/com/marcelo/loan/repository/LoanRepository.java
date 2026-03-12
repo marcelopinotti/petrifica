ce0620323b6f04c51cccdf4abf521a1d1e588f2d
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-03-11 23:34:08 -0300

    feat: add methods to LoanRepository for querying loans by customer ID and status

--- loan-service/src/main/java/com/marcelo/loan/repository/LoanRepository.java
+++ loan-service/src/main/java/com/marcelo/loan/repository/LoanRepository.java
@@ -1,7 +1,13 @@
 package com.marcelo.loan.repository;
 
 import com.marcelo.loan.entity.Loan;
+import com.marcelo.loan.entity.enums.LoanStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface LoanRepository extends MongoRepository<Loan, String> {
+    List<Loan> findByCustomerId(String customerId);
+    List<Loan> findByStatus(LoanStatus status);
+    boolean existsByCustomerIdAndStatusIn(String customerId, List<LoanStatus> statuses);
 }
