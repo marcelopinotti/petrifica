ac26130a896824834ecd8acec4d2a18b7978e7ef
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-03-12 00:15:14 -0300

    feat: add MongoConfig class for enabling MongoDB auditing

---
+++ fraud-analysis-service/src/main/java/com/marcelo/fraud/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.marcelo.fraud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
