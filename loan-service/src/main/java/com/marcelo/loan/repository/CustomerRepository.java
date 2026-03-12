6bb17c128ad921903f68ddaaf30f4368876280c4
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-03-11 23:34:18 -0300

    feat: add methods to CustomerRepository for querying customers by Keycloak ID, CPF, and email

--- loan-service/src/main/java/com/marcelo/loan/repository/CustomerRepository.java
+++ loan-service/src/main/java/com/marcelo/loan/repository/CustomerRepository.java
@@ -3,5 +3,10 @@ package com.marcelo.loan.repository;
 import com.marcelo.loan.entity.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends MongoRepository<Customer, String> {
+    Optional<Customer> findByKeycloakId(String keycloakId);
+    Optional<Customer> findByCpf(String cpf);
+    Optional<Customer> findByEmail(String email);
 }
