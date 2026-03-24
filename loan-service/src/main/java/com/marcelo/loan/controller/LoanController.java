7e473923c0bef7c62a63be0c00efb10657f320da
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-03-23 21:49:45 -0300

    feat: add LoanController with RESTful endpoints for loan management

--- loan-service/src/main/java/com/marcelo/loan/controller/LoanController.java
+++ loan-service/src/main/java/com/marcelo/loan/controller/LoanController.java
@@ -1,4 +1,27 @@
 package com.marcelo.loan.controller;
 
+import com.marcelo.loan.controller.request.LoanRequest;
+import com.marcelo.loan.controller.response.LoanResponse;
+import com.marcelo.loan.service.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/loans")
+@RequiredArgsConstructor
 public class LoanController {
 }
