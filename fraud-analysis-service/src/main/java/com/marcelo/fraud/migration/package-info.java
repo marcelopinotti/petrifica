d25e065283b154e2ef2188ed8db1dfc22d255bdd
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-04-04 23:35:05 -0300

    feat: add package-info for Mongock migrations in loan and fraud analysis services

---
+++ fraud-analysis-service/src/main/java/com/marcelo/fraud/migration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Pacote reservado para as migrations do Mongock no fraud-analysis-service.
+ */
+package com.marcelo.fraud.migration;
+
---
+++ loan-service/src/main/java/com/marcelo/loan/migration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Pacote reservado para as migrations do Mongock no loan-service.
+ */
+package com.marcelo.loan.migration;
+
