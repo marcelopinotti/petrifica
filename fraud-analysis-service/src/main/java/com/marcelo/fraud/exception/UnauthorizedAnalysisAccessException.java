2f8aeb390ebeaff5ff73c91398e7d6b0423f57f8
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-04-26 20:21:33 -0300

    feat: add custom exception classes for customer existence and unauthorized access in fraud analysis

---
+++ fraud-analysis-service/src/main/java/com/marcelo/fraud/exception/UnauthorizedAnalysisAccessException.java
@@ -0,0 +1,7 @@
+package com.marcelo.fraud.exception;
+
+public class UnauthorizedAnalysisAccessException extends RuntimeException {
+    public UnauthorizedAnalysisAccessException(String message) {
+        super(message);
+    }
+}
---
+++ loan-service/src/main/java/com/marcelo/loan/exception/CustomerAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.marcelo.loan.exception;
+
+public class CustomerAlreadyExistsException extends RuntimeException {
+    public CustomerAlreadyExistsException(String message) {
+        super(message);
+    }
+}
